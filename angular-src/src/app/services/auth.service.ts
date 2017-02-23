68ef2174dfe017260b915528be57cc39df7ff39d
Stefan Blattmann <stefan.blattmann@gmail.com>
2017-02-23 15:01:19 +0100

    Implement protected area
    * Add auth guard

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
-// import {tokenNotExpired} from 'angular2-jwt';
+import { tokenNotExpired } from 'angular2-jwt';
 
 @Injectable()
 export class AuthService {
   authToken: any;
   user: any;
 
-  constructor(private http:Http) { }
+  constructor(private http: Http) { }
 
   // Register user
   registerUser(user) {
@@ -26,12 +26,33 @@ export class AuthService {
     return this.http.post(authenticateUrl, user, {headers: headers}).map(res => res.json());
   }
 
+  // Get user profile
+  getProfile() {
+    let headers = new Headers();
+    let profileUrl = 'http://localhost:3000/users/profile';
+    this.loadToken();
+    headers.append('Authorization', this.authToken);
+    headers.append('Content-Type','application/json');
+    return this.http.get(profileUrl, {headers: headers}).map(res => res.json());
+  }
+
   // Store user data
   storeUserData(token, user) {
     localStorage.setItem('id_token', token);
     localStorage.setItem('user', JSON.stringify(user));
     this.authToken = token;
     this.user = user;
+  }
+
+  // Load token
+  loadToken() {
+    const token = localStorage.getItem('id_token');
+    this.authToken = token;
+  }
+
+  // Verify token
+  loggedIn() {
+    return tokenNotExpired();
   }
 
   // Logout user
