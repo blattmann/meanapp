04d3f091ff2b86062fd6530e234526c8e49b5a88
Stefan Blattmann <stefan.blattmann@gmail.com>
2017-02-23 14:03:52 +0100

    Add Login and logout
    * Add Login functionality
    * Add Logout functionality
    * Store data to local storage

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
+// import {tokenNotExpired} from 'angular2-jwt';
 
 @Injectable()
 export class AuthService {
@@ -9,10 +10,34 @@ export class AuthService {
 
   constructor(private http:Http) { }
 
+  // Register user
   registerUser(user) {
     let headers = new Headers();
     let registerUrl = 'http://localhost:3000/users/register';
     headers.append('ContentType', 'application/json');
     return this.http.post(registerUrl, user, {headers: headers}).map(res => res.json());
+  }
+
+  // Authenticate user
+  authenticateUser(user) {
+    let headers = new Headers();
+    let authenticateUrl = 'http://localhost:3000/users/authenticate';
+    headers.append('Content-Type', 'application/json');
+    return this.http.post(authenticateUrl, user, {headers: headers}).map(res => res.json());
+  }
+
+  // Store user data
+  storeUserData(token, user) {
+    localStorage.setItem('id_token', token);
+    localStorage.setItem('user', JSON.stringify(user));
+    this.authToken = token;
+    this.user = user;
+  }
+
+  // Logout user
+  logout() {
+    this.authToken = null;
+    this.user = null;
+    localStorage.clear();
   }
 }
